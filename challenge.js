bf5786d4f6344f8a905e52b8117eadeeeafb6417
bayuaji17 <nugroho.bayuaji17@gmail.com>
2023-09-09 02:34:01 +0700

    first commit

--- challenge.js
+++ challenge.js
@@ -1,14 +1,15 @@
 // NOMOR 1
+console.log("=========================")
 console.log("NOMOR 1 \n")
 const kalimat1 = 'Andini sangat mencintai kamu selamanya'
 const kalimat2 = 'gunung bromo tak akan mampu menggambarkan besarnya cintaku padamu'
 
 function changeWord(selectedText, changedText, text) {
-  // Menggunakan metode replace untuk mengganti kata
-  //   const newText = text.replace(new RegExp(selectedText, 'g'), changedText);
-  const newText = text.replace(selectedText, changedText);
-  // mengembalikan nilai newText
-  return newText;
+    // Menggunakan metode replace untuk mengganti kata
+    //   const newText = text.replace(new RegExp(selectedText, 'g'), changedText);
+    const newText = text.replace(selectedText, changedText);
+    // mengembalikan nilai newText
+    return newText;
 
 }
 console.log(changeWord('mencintai', 'membeci', kalimat1))
@@ -18,18 +19,18 @@ console.log(changeWord('bromo', 'semeru', kalimat2))
 // 'Gunung sangat membenci kamu selamanya'
 
 // NOMOR 2
+console.log("=========================")
 console.log("\nNOMOR 2 \n")
-
 const checkTypeNumber = (givenNumber) => {
-  if (givenNumber == undefined) {
-    return "error: bro where is the parameter ?"
-  } else if (typeof givenNumber != "number") {
-    return "error: Invalid data type"
-  } else if (givenNumber % 2 == 0) {
-    return "genap"
-  } else {
-    return "ganjil"
-  }
+    if (givenNumber == undefined) {
+        return "error: bro where is the parameter ?"
+    } else if (typeof givenNumber != "number") {
+        return "error: Invalid data type"
+    } else if (givenNumber % 2 == 0) {
+        return "genap"
+    } else {
+        return "ganjil"
+    }
 }
 
 //EXpected Result
@@ -42,21 +43,21 @@ console.log(checkTypeNumber()) //Output yang keluar => "error : bro where is the
 
 
 // NOMOR 3
+console.log("=========================")
 console.log("\nNOMOR 3 \n")
-
 // Buat Function
 function getAngkaTerbesarKedua(bayu) {
-  // Jika inputannya kosong/undefined maka error
-  if (typeof (bayu) === "undefined") {
-    return "Error : Karena undefined atau tidak ada inputan"
-    // jika inputannya bukan array maka error 
-  } else if (!Array.isArray(bayu)) {
-    return "Error : Karena bukan array tetapi number"
-  }
-  // Sorting/ mengurutkan dari besar ke kecil
-  // lalu mengambil nilai ke 2 yaitu index 1
-  bayu.sort((a, b) => b - a)
-  return bayu[1]
+    // Jika inputannya kosong/undefined maka error
+    if (typeof (bayu) === "undefined") {
+        return "Error : Karena undefined atau tidak ada inputan"
+        // jika inputannya bukan array maka error 
+    } else if (!Array.isArray(bayu)) {
+        return "Error : Karena bukan array tetapi number"
+    }
+    // Sorting/ mengurutkan dari besar ke kecil
+    // lalu mengambil nilai ke 2 yaitu index 1
+    bayu.sort((a, b) => b - a)
+    return bayu[1]
 }
 const dataAngka = [9, 4, 7, 7, 4, 3, 2, 2, 8]
 console.log(getAngkaTerbesarKedua(dataAngka))
@@ -64,126 +65,146 @@ console.log(getAngkaTerbesarKedua(2)) // BUKAN ARRAY
 console.log(getAngkaTerbesarKedua()) // KOSONG 
 
 // Nomor 4
+console.log("=========================")
 console.log("\nNOMOR 4 \n")
-
 const dataPenjualanPakAldi = [
-  {
-    namaProduct: 'sepatu Futsal Nike Vapor Academy 8',
-    hargaSatuan: 760000,
-    kategori: "Sepatu Sport",
-    totalTerjual: 90,
-  },
-  {
-    namaProduct: 'Sepatu Warrior Tristan Black Brown High',
-    hargaSatuan: 960000,
-    kategori: "Sepatu Sneaker",
-    totalTerjual: 37,
-  },
-  {
-    namaProduct: 'Sepatu Warrior Tristan Maroon High',
-    hargaSatuan: 360000,
-    kategori: "Sepatu Sneaker",
-    totalTerjual: 90,
-  },
-  {
-    namaProduct: 'Sepatu Warrior Rainbow Tosca Corduroy',
-    hargaSatuan: 120000,
-    kategori: "Sepatu Sneaker",
-    totalTerjual: 90,
-  },
+    {
+        namaProduct: 'sepatu Futsal Nike Vapor Academy 8',
+        hargaSatuan: 760000,
+        kategori: "Sepatu Sport",
+        totalTerjual: 90,
+    },
+    {
+        namaProduct: 'Sepatu Warrior Tristan Black Brown High',
+        hargaSatuan: 960000,
+        kategori: "Sepatu Sneaker",
+        totalTerjual: 37,
+    },
+    {
+        namaProduct: 'Sepatu Warrior Tristan Maroon High',
+        hargaSatuan: 360000,
+        kategori: "Sepatu Sneaker",
+        totalTerjual: 90,
+    },
+    {
+        namaProduct: 'Sepatu Warrior Rainbow Tosca Corduroy',
+        hargaSatuan: 120000,
+        kategori: "Sepatu Sneaker",
+        totalTerjual: 90,
+    },
 ]
 function hitungTotalPenjualan(dataPenjualan) {
 
-  if (typeof dataPenjualan !== "object") {
-    return "Error BRO"
-  }
+    if (typeof dataPenjualan !== "object") {
+        return "Error BRO"
+    }
 
-  let totalTerjualArr = dataPenjualan.map((value) => {
-    return value.totalTerjual
-  })
+    let totalTerjualArr = dataPenjualan.map((value) => {
+        return value.totalTerjual
+    })
 
-  let hasilTotalTerjual = totalTerjualArr.reduce((defaultValue, firstValue) => {
-    return defaultValue + firstValue
-  }, 0)
+    let hasilTotalTerjual = totalTerjualArr.reduce((defaultValue, firstValue) => {
+        return defaultValue + firstValue
+    }, 0)
 
-  return hasilTotalTerjual
+    return hasilTotalTerjual
 
 }
 // console.log(hitungTotalPenjualan(1))
 console.log(hitungTotalPenjualan(dataPenjualanPakAldi))
 
+console.log("=========================")
 console.log("\nNOMOR 5 \n")
-
 const dataPenjualanNovel = [
-  {
-    idProduct: 'BOOK002421',
-    namaProduk: 'Pulang - Pergi',
-    penulis: 'Tere Liye',
-    hargaBeli: 60000,
-    hargaJual: 86000,
-    totalTerjual: 150,
-    sisaStok: 17,
-  },
-  {
-    idProduct: 'BOOK002351',
-    namaProduk: 'Selamat Tinggal',
-    penulis: 'Tere Liye',
-    hargaBeli: 75000,
-    hargaJual: 103000,
-    totalTerjual: 171,
-    sisaStok: 20,
-  },
-  {
-    idProduct: 'BOOK002941',
-    namaProduk: 'Garis Waktu',
-    penulis: 'Fiersa Besari',
-    hargaBeli: 67000,
-    hargaJual: 99000,
-    totalTerjual: 213,
-    sisaStok: 5,
-  },
-  {
-    idProduct: 'BOOK002941',
-    namaProduk: 'Laskar Pelangi',
-    penulis: 'Andrea Hirata',
-    hargaBeli: 55000,
-    hargaJual: 68000,
-    totalTerjual: 20,
-    sisaStok: 56,
-  },
+    {
+        idProduct: 'BOOK002421',
+        namaProduk: 'Pulang - Pergi',
+        penulis: 'Tere Liye',
+        hargaBeli: 60000,
+        hargaJual: 86000,
+        totalTerjual: 150,
+        sisaStok: 17,
+    },
+    {
+        idProduct: 'BOOK002351',
+        namaProduk: 'Selamat Tinggal',
+        penulis: 'Tere Liye',
+        hargaBeli: 75000,
+        hargaJual: 103000,
+        totalTerjual: 171,
+        sisaStok: 20,
+    },
+    {
+        idProduct: 'BOOK002941',
+        namaProduk: 'Garis Waktu',
+        penulis: 'Fiersa Besari',
+        hargaBeli: 67000,
+        hargaJual: 99000,
+        totalTerjual: 213,
+        sisaStok: 5,
+    },
+    {
+        idProduct: 'BOOK002941',
+        namaProduk: 'Laskar Pelangi',
+        penulis: 'Andrea Hirata',
+        hargaBeli: 55000,
+        hargaJual: 68000,
+        totalTerjual: 20,
+        sisaStok: 56,
+    },
 ];
-
-// totalKeuntungan = hargaJual - hargaBeli
-// totalModal = hargaBeli
-// produkBukuTerlaris = totalTerjual
-// persentaseKeuntungan = 
-// penulisTerlaris = penulis
 
 
 function getInfoPenjualan(dataPenjualanBuku) {
-  // Keuntungan
-  let totalKeuntunganArr = dataPenjualanBuku.map((value) => {
-    return (value.hargaJual - value.hargaBeli) * value.totalTerjual
-  })
+    if (typeof dataPenjualanBuku !== "object") {
+        return "Error BRO"
+    }
 
-  let totalKeuntungan = totalKeuntunganArr.reduce((defaultValue, firstValue) => {
-    return defaultValue + firstValue
-  }, 0)
-  
-  // Modal
-  let modalUsaha = dataPenjualanBuku.map((value) => {
-    return value.hargaBeli * (value.sisaStok + value.totalTerjual)
-  })
-  let totalModal = modalUsaha.reduce((defaultValue, firstValue) => {
-    return defaultValue + firstValue
-  }, 0)
+    const formatCurrency = (jumlah) =>
+        new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(jumlah);
+    // totalKeuntungan
+    let totalKeuntunganArr = dataPenjualanBuku.map((value) => {
+        return (value.hargaJual - value.hargaBeli) * value.totalTerjual
+    })
 
+    let totalKeuntungan = totalKeuntunganArr.reduce((defaultValue, firstValue) => {
+        return defaultValue + firstValue
+    }, 0)
 
-  return {
-    totalKeuntungan,
-    totalModal
-  }
+    // totalModal
+    let modalUsaha = dataPenjualanBuku.map((value) => {
+        return value.hargaBeli * (value.sisaStok + value.totalTerjual)
+    })
+    let totalModal = modalUsaha.reduce((defaultValue, firstValue) => {
+        return defaultValue + firstValue
+    }, 0)
+    // persentaseKeuntungan
+    let totalJual = dataPenjualanBuku.map((value) => {
+        return value.hargaJual * value.totalTerjual
+    })
+    let totalPenjualanFinal = totalJual.reduce((defaultValue, firstValue) => {
+        return defaultValue + firstValue
+    })
+    // Perubahan format persen
+    let persentase = ((totalPenjualanFinal - totalModal) / totalModal) * 100
+    const persentaseKeuntungan = persentase.toFixed(2) + "%"
+    // produk Buku terlaris
+    dataPenjualanBuku.sort((a, b) => {
+        return b.totalTerjual - a.totalTerjual;
+    });
 
+    const bukuTerlaris = dataPenjualanBuku[0];
+
+    const produkBukuTerlaris = bukuTerlaris.namaProduk;
+    const penulisTerlaris = bukuTerlaris.penulis;
+
+    return {
+        totalKeuntungan: formatCurrency(totalKeuntungan),
+        totalModal: formatCurrency(totalModal),
+        persentaseKeuntungan,
+        produkBukuTerlaris,
+        penulisTerlaris
+    }
 
 }
 
